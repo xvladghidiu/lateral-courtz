85bafd618fdb9178598872b7e7d9dadc85e9bef3
xvladghidiu <xvladghidiu@users.noreply.github.com>
2026-04-05 22:22:16 +0300

    fix: rename Active label to Active Courts

--- client/src/components/ShotClockRow.tsx
+++ client/src/components/ShotClockRow.tsx
@@ -20,7 +20,7 @@ export default function ShotClockRow({ sessions, courts }: ShotClockRowProps) {
   return (
     <div className="absolute top-1/2 left-5 -translate-y-1/2 z-10 flex flex-col gap-5">
       <ShotClock value={currentlyPlaying(sessions)} label="Playing Now" color="red" />
-      <ShotClock value={sessions.length} label="Active" color="red" />
+      <ShotClock value={sessions.length} label="Active Courts" color="red" />
       <ShotClock value={almostFullCount(sessions)} label="Filling" color="orange" />
       <ShotClock value={courts.length} label="Nearby Courts" color="green" />
     </div>
